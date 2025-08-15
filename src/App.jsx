34393d5905d796041fa6414fe91dc7b0a94bd009
Anmol840 <ag6939541@gmail.com>
2025-08-16 01:39:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,9 @@ import Nav from "./Components/Nav/Nav";
 import Index from "./Components/Page/Index";
 import Footer from "./Components/Footer/Footer";
 import Achievements from "./Components/Achievements/Achievements";
-import logo from "./assets/logo.png";
-import applogo from "./assets/gears-138199.gif";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 3000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-        <img src={logo} alt="Resonance Racing" className="w-64 mb-6" />
-        <img src={applogo} alt="Loading animation" className="h-30 w-[120px]" />
-      </div>
-    );
-  }
+  
 
   return (
     <>
